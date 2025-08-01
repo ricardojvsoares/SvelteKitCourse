e00d005d2987219c5de619fdf1220ac0f507aec0
Ricardo Soares <ricardo.soares@oddsgate.com>
2025-08-01 10:22:43 +0100

    Section 2

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,14 +1,135 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
+
+	type Props = (
+		| (HTMLButtonAttributes & { href?: never })
+		| (HTMLAnchorAttributes & { href: string })
+	) & {
+		left?: Snippet<[boolean]>;
+		right?: Snippet<[boolean]>;
+		children: Snippet;
+		size?: 'sm' | 'lg';
+		shadow?: boolean;
+		onLeftHover?: () => void;
+		/* 		bgColor?: string;
+		textColor?: string; */
+	};
+	let {
+		/* 		bgColor,
+		textColor, */
+		left,
+		right,
+		children,
+		onLeftHover,
+		size = 'sm',
+		shadow = false,
+		...props
+	}: Props = $props();
+
+	let isLeftHover: boolean = $state(false);
+	let isRightHover: boolean = $state(false);
+	let button: HTMLButtonElement | HTMLAnchorElement;
+
+	export function focus() {
+		button.focus();
+	}
+	export function getButton() {
+		return button;
+	}
 </script>
 
 <h1>Button</h1>
+
+<!-- 
 {#snippet sum(a: number, b: number)}
 	{#snippet bold(x: any)}
 		<b>{x}</b>
 	{/snippet}
 	<span>{a}+{b} = {@render bold(a + b)}</span>
 {/snippet}
+ -->
 
-<button>Text</button>
+<!--  style:--buttonBgColor={bgColor}
+ style:--buttonTextColor={textColor} -->
+<svelte:element
+	this={props.href ? 'a' : 'button'}
+	bind:this={button}
+	{...props}
+	class={{ sm: size === 'sm', lg: size === 'lg', shadow }}
+>
+	{#if left}
+		<div
+			role="presentation"
+			class="left-content"
+			onmouseenter={() => {
+				onLeftHover?.();
+				isLeftHover = true;
+			}}
+			onmouseleave={() => {
+				isLeftHover = false;
+			}}
+		>
+			{@render left?.(isLeftHover)}
+		</div>
+	{/if}
+	{@render children()}
 
-{@render sum(2, 2)}
+	{#if right}
+		<div
+			role="presentation"
+			class="right-content"
+			onmouseenter={() => {
+				isRightHover = true;
+			}}
+			onmouseleave={() => {
+				isRightHover = false;
+			}}
+		>
+			{@render right?.(isRightHover)}
+		</div>
+	{/if}
+</svelte:element>
+
+<!-- {@render sum(2, 2)} -->
+<style lang="scss">
+	button {
+		border: none;
+		background-color: var(--buttonBgColor, #ff3e00);
+		color: var(--buttonTextColor, #ffffff);
+		padding: 0 20px;
+		height: 45px;
+		font-weight: bold;
+		border-radius: 5px;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		&:disabled {
+			opacity: 0.6;
+			cursor: not-allowed;
+		}
+		&:hover {
+			background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
+		}
+		&:active {
+			background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+		}
+		&.sm {
+			height: 45px;
+		}
+		&.lg {
+			height: 55px;
+			font-size: 20px;
+		}
+		&.shadow {
+			box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
+		}
+		.left-content {
+			margin-inline-end: 10px;
+		}
+		.right-content {
+			margin-inline-start: 10px;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,63 @@
 	import Counter from '$lib/components/Counter.svelte';
 	import DisplayName from '$lib/components/DisplayName.svelte';
 	import RandomNumber from '$lib/components/RandomNumber.svelte';
+
+	import { AlarmClock, Search } from 'lucide-svelte';
+	let button: Button;
+	$effect(() => {
+		button.getButton().focus();
+	});
 </script>
 
-<DisplayName />
+<!-- <DisplayName />
+
 <RandomNumber />
-<Counter />
-<Button />
+
+<Counter /> -->
+
+<div
+	role="presentation"
+	onclickcapture={(e) => {
+		e.stopPropagation();
+		console.log('Event coming from div');
+	}}
+>
+	<Button
+		href={'https://google.com'}
+		bind:this={button}
+		onclickcapture={(e) => {
+			e.stopPropagation();
+			console.log('Event coming from Button');
+		}}
+		onLeftHover={() => {
+			console.log('Left hovered');
+		}}
+		size="sm"
+		--buttonBgColor="yellow"
+		--buttonTextColor="red"
+		>{#snippet left(isHover: boolean)}
+			{#if isHover}
+				<Search />
+			{:else}
+				L
+			{/if}
+		{/snippet}
+		Text
+		{#snippet right(isHover: boolean)}
+			{#if isHover}
+				<AlarmClock />
+			{:else}
+				R
+			{/if}
+		{/snippet}
+	</Button>
+</div>
+
+<style>
+	/* :global(body) {
+		background-color: red;
+	} */
+	:global(button) {
+		font-family: 'Courier New', Courier, monospace;
+	}
+</style>
